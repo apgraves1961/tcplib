65062aaad4376b643c7009f5b57c45a92f64e8f4
apgraves1961 <apgraves1961@users.noreply.github.com>
2017-03-07 01:57:58 -0500

    Silly change

--- tcpsockops.h
+++ tcpsockops.h
@@ -1,7 +1,7 @@
-#ifndef TCPSOCKOPS_H
-#define TCPSOCKOPS_H 1
+#ifndef TCP_SOCKOPS_H
+#define TCP_SOCKOPS_H 1
 
 int tcp_writetosocket (int, char *, int, int);
 int tcp_readfromsocket (int, char *, int, int);
 
-#endif // TCPSOCKOPS_H
+#endif // TCP_SOCKOPS_H
